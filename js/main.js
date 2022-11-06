18f72896ab4609b1bf51ce185909b9300f433d68
DizMaxWork <solskype01@gmail.com>
2022-11-07 00:20:54 +0300

    Доработка

--- js/main.js
+++ js/main.js
@@ -43,6 +43,22 @@ $(document).ready(function (){
             }
         ]
     })
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true,
+        infinite: false
+      });
 });
 
 // $('.slick').on('afterChange', function(event, slick, currentSlide, nextSlide){
