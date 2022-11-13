c6deabf47816e631413f415793ab041d8c851000
Mrrockmanish <mrrockmanish@gmail.com>
2022-11-14 02:57:32 +0300

    мастштабные правки

--- js/main.js
+++ js/main.js
@@ -49,22 +49,9 @@ $(document).ready(function (){
             }
         ]
     })
-    $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-nav'
-      });
-      $('.slider-nav').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        dots: false,
-        centerMode: true,
-        focusOnSelect: true,
-        infinite: false
-      });
+
+
+
 });
 
 // $('.slick').on('afterChange', function(event, slick, currentSlide, nextSlide){
